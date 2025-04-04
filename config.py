25b3ed78cb14d5c671cb1ce78a5e9d327c129a4d
Dzigojty <dzigojty@mail.ru>
2025-04-04 09:25:46 +0300

    Создал конфиг файл config.py Установил (pip install pydantic) и (pip install pydantic-settings)

---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+
+class Settings(BaseSettings):
+    SERVER_HOST: str
+    SERVER_PORT: int
+
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+
+    UPLOAD_DIR: str = "uploads"
+
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+    BASE_URL = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
+    EMOJI = "😊"
+
+    model_config = ConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+settings = Settings()
